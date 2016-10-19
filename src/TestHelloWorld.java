0d284ddd0c40c3c3c7ac3d7d00d0cace1a7d45ab
luohuan <345778312@qq.com>
2016-10-19 10:45:38 +0800

    Initial commit

---
+++ src/TestHelloWorld.java
@@ -0,0 +1,11 @@
+/**
+ * Created by 34577 on 2016/10/18.
+ */
+public class TestHelloWorld {
+    public static void main(String[] args) {
+        System.out.println("你好IDEA");
+        System.out.println("你好IDEA");
+        System.out.println("你好IDEA");
+        System.out.println("你好IDEA");
+    }
+}
